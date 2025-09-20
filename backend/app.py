d815b2238a607c8fde48326e54ddca36915d80af
faizbepari19 <faizbepari19@gmail.com>
2025-09-20 19:00:35 +0530

    Chnages made for faster deployments

--- backend/app.py
+++ backend/app.py
@@ -95,7 +95,9 @@ def create_app():
     
     return app
 
+# For Gunicorn compatibility
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
