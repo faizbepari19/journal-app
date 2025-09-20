4150c2bd3285cbfacb4441f3f90e07c0ae0b08b2
faizbepari19 <faizbepari19@gmail.com>
2025-09-20 19:07:30 +0530

    Revert "Chnages made for faster deployments"
    This reverts commit d815b2238a607c8fde48326e54ddca36915d80af.

--- backend/app.py
+++ backend/app.py
@@ -95,9 +95,7 @@ def create_app():
     
     return app
 
-# For Gunicorn compatibility
-app = create_app()
-
 if __name__ == '__main__':
+    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
